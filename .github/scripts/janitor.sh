d3420561ebafdc97838f6be9c250fa60ad13dfe6
Thomas Lorntzen <cyrthomas1@gmail.com>
2020-01-10 18:29:39 +0100

    Try fetching all branches

--- .github/scripts/janitor.sh
+++ .github/scripts/janitor.sh
@@ -1,4 +1,10 @@
 echo "Janitoring away!"
 echo "Do I have access to environment variables? $GITHUB_WORKSPACE"
 echo "What happens if I run git?"
+git fetch --no-tags --prune --depth=1 origin +refs/heads/*:refs/remotes/origin/*
 git branch
+
+# FOLDER=/somefolder
+# if [ -d "$FOLDER" ]; then
+#     echo "$FOLDER is a directory"
+# fi
