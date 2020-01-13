7f27b670dba417912d99f1eb95cac2a1ec8f88e0
Thomas Lorntzen <cyrthomas1@gmail.com>
2020-01-13 15:02:26 +0100

    Rework janitor into delete event instead of cron

--- .github/scripts/janitor.sh
+++ .github/scripts/janitor.sh
@@ -1,10 +1,3 @@
 echo "Janitoring away!"
 echo "Do I have access to environment variables? $GITHUB_WORKSPACE"
-echo "What happens if I run git?"
-git fetch --no-tags --prune --depth=1 origin +refs/heads/*:refs/remotes/origin/*
-git branch
-
-# FOLDER=/somefolder
-# if [ -d "$FOLDER" ]; then
-#     echo "$FOLDER is a directory"
-# fi
+echo "Branch or tag ref that triggered event was: $GITHUB_REF"
