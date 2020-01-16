cc5a1285774b34bbc8181a44e3dd396ce8df21a4
Thomas Lorntzen <cyrthomas1@gmail.com>
2020-01-16 15:36:47 +0100

    Teinker Tienker

--- .github/scripts/script.sh
+++ .github/scripts/script.sh
@@ -1,13 +1,9 @@
-echo Hello from within script... does script have brand? $BRAND
-
-echo $BRAND_NAME
-
 if [ "$BRAND" == "payex" ]; then
-    echo ::set-env name=BRAND_NAME::PayEx
     echo ::set-env name=BRAND_URL::https://payexdesignguide.z16.web.core.windows.net
+    echo ::set-env name=BRAND_BUILD:::payex
 elif [ "$BRAND" == "swedbankpay" ]; then
-    echo ::set-env name=BRAND_NAME::Swedbank Pay
     echo ::set-env name=BRAND_URL::https://swedbankpaydesignguide.z16.web.core.windows.net
+    # Swedbank Pay brand doesn't need a brand build variable
 else
     echo "Unknown brand!"
 fi
