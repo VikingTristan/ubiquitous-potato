07c288472d9e5b537277ea98bb64ab15d66b11c9
Thomas Lorntzen <cyrthomas1@gmail.com>
2020-01-16 14:32:03 +0100

    Tinker

--- .github/scripts/script.sh
+++ .github/scripts/script.sh
@@ -1,1 +1,4 @@
-echo Hello from within script... does script have brand aswell? $BRAND
+echo Hello from within script... does script have brand? $BRAND
+echo ::set-env name=BRAND_NAME::$BRAND
+echo ::set-env name=BRAND_URL::testurl
+echo $BRAND_NAME
