948c5e29764996e0a1da6ce88514bdee49c6ceeb
Thomas Lorntzen <cyrthomas1@gmail.com>
2020-01-16 14:37:11 +0100

    Tinker

--- .github/scripts/script.sh
+++ .github/scripts/script.sh
@@ -2,3 +2,11 @@ echo Hello from within script... does script have brand? $BRAND
 echo ::set-env name=BRAND_NAME::$BRAND
 echo ::set-env name=BRAND_URL::testurl
 echo $BRAND_NAME
+
+if [ "$BRAND" -eq "payex" ]; then
+    echo "We should do payex things now!"
+elif [ "$BRAND" -eq "swedbankpay" ]; then
+    echo "We should do swedbank pay things now!"
+else
+    echo "Unknown brand!"
+fi
