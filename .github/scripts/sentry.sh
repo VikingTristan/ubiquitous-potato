bce11e53c47da8a842a3706481464705a3894f03
Thomas Lorntzen <cyrthomas1@gmail.com>
2020-01-27 14:00:55 +0100

    Update stuff

---
+++ .github/scripts/sentry.sh
@@ -0,0 +1,5 @@
+echo "I should be replacing token here"
+ls
+
+# This replaces abc with XYZ, this is what we should do with the "<token>" string in the .sentryclirc file
+# sed -i -e 's/abc/XYZ/g' /tmp/file.txt
