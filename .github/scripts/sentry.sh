a09924cd1e0ac1564623cee2b53a51b564a84113
Thomas Lorntzen <cyrthomas1@gmail.com>
2020-01-27 14:52:53 +0100

    J.R. Token

--- .github/scripts/sentry.sh
+++ .github/scripts/sentry.sh
@@ -3,4 +3,4 @@ ls
 
 # This replaces <token> with our $TOKEN variable, this is what we should do with the "<token>" string in the .sentryclirc file
 # sed -i -e 's/<token>/$TOKEN/g' .sentryclirc
-sed -i -e 's/tokenhere/$TOKEN/g' ./src/index.html
+sed -i -e 's/tokenhere/'"$TOKEN"'/g' ./src/index.html
