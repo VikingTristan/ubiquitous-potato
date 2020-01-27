f38e98dc3c7436595ca18e24351b95bce1bce528
Thomas Lorntzen <cyrthomas1@gmail.com>
2020-01-27 14:42:39 +0100

    Token test

--- .github/scripts/sentry.sh
+++ .github/scripts/sentry.sh
@@ -1,5 +1,6 @@
 echo "I should be replacing token here"
 ls
 
-# This replaces abc with XYZ, this is what we should do with the "<token>" string in the .sentryclirc file
-# sed -i -e 's/abc/XYZ/g' /tmp/file.txt
+# This replaces <token> with our $TOKEN variable, this is what we should do with the "<token>" string in the .sentryclirc file
+# sed -i -e 's/<token>/$TOKEN/g' .sentryclirc
+sed -i -e 's/tokenhere/$TOKEN/g' ./src/index.html
