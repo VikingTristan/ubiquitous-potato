2031b89606bf000c0b5eece1485cbeb278a18b2b
Thomas Lorntzen <cyrthomas1@gmail.com>
2020-01-27 15:02:53 +0100

    Setup sentry token in master workflow

--- .github/scripts/sentry.sh
+++ .github/scripts/sentry.sh
@@ -1,6 +1,2 @@
-echo "I should be replacing token here"
-ls
-
-# This replaces <token> with our $TOKEN variable, this is what we should do with the "<token>" string in the .sentryclirc file
-# sed -i -e 's/<token>/$TOKEN/g' .sentryclirc
-sed -i -e 's/tokenhere/'"$TOKEN"'/g' ./src/index.html
+# This replaces the .sentryclirc <token> string with our $TOKEN variable from GitHub secrets
+sed -i -e 's/<token>/'"$TOKEN"'/g' ./.sentryclirc
