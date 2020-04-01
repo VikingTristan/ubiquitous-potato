11b0efe28cdd30b4fd708824141816f7ca3c639e
Thomas Lorntzen <cyrthomas1@gmail.com>
2020-04-01 12:14:09 +0200

    Lets try another release

--- .github/scripts/variables.sh
+++ .github/scripts/variables.sh
@@ -1,8 +1,10 @@
 if [ "$BRAND" == "payex" ]; then
+    echo ::set-output name=BRAND_NAME::PayEx
     echo ::set-output name=BRAND_URL::https://payexdesignguidetest.z6.web.core.windows.net
     echo ::set-output name=AZURE_ACCOUNT::payexdesignguidetest
     echo ::set-output name=BUILD_SCRIPT::build:prod:payex
 elif [ "$BRAND" == "swedbankpay" ]; then
+    echo ::set-output name=BRAND_NAME::SwedbankPay
     echo ::set-output name=BRAND_URL::https://swedpaydesignguidetest.z6.web.core.windows.net
     echo ::set-output name=AZURE_ACCOUNT::swedpaydesignguidetest
     echo ::set-output name=BUILD_SCRIPT::build:prod
