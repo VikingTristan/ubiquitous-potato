927aa8d5dcf1c54a9eaf09fdb80b61608b5a6876
Thomas Lorntzen <cyrthomas1@gmail.com>
2020-07-15 17:04:15 +0200

    test

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,26 @@
 module.exports = {
-    projects: ["<rootDir>/src/app", "<rootDir>/src/scripts"],
+    // projects: ["<rootDir>/src/app", "<rootDir>/src/scripts"],
+    moduleNameMapper: {
+        "\\.(svg)$": "./__mocks__/svgMock.js"
+    },
     collectCoverage: true,
+    collectCoverageFrom: [
+        "**/*.{js,jsx}"
+    ],
     coverageDirectory: "coverage",
     coverageReporters: [
         "json",
         "lcov",
         "text-summary"
+    ],
+    setupFiles: [
+        "./jest.setup.js"
+    ],
+    snapshotSerializers: [
+        "enzyme-to-json/serializer"
+    ],
+    testPathIgnorePatterns: [
+        "\\.spec\\.js",
+        "./Documentation/utils/ComponentPreview/"
     ]
 };
