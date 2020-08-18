4a3f12e5492916aeae61d3d2ead8e00d02f20317
Thomas Lorntzen <cyrthomas1@gmail.com>
2020-08-18 15:29:43 +0200

    Test urls

--- .github/scripts/variables.sh
+++ .github/scripts/variables.sh
@@ -1,12 +1,12 @@
 if [ "$BRAND" == "payex" ]; then
     echo ::set-output name=BRAND_NAME::PayEx
-    echo ::set-output name=BRAND_URL::https://payexdesignguide.z6.web.core.windows.net
-    echo ::set-output name=AZURE_ACCOUNT::payexdesignguide
+    echo ::set-output name=BRAND_URL::https://payexdesignguidetest.z6.web.core.windows.net
+    echo ::set-output name=AZURE_ACCOUNT::payexdesignguidetest
     echo ::set-output name=BUILD_SCRIPT::build:prod:payex
 elif [ "$BRAND" == "swedbankpay" ]; then
     echo ::set-output name=BRAND_NAME::SwedbankPay
-    echo ::set-output name=BRAND_URL::https://swedpaydesignguide.z6.web.core.windows.net
-    echo ::set-output name=AZURE_ACCOUNT::swedbankpaydesignguide
+    echo ::set-output name=BRAND_URL::https://swedpaydesignguidetest.z6.web.core.windows.net
+    echo ::set-output name=AZURE_ACCOUNT::swedpaydesignguidetest
     echo ::set-output name=BUILD_SCRIPT::build:prod
 else
     echo "Unknown brand '$BRAND'!"
